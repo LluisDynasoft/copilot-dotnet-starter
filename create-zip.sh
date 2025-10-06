83819332b89573d6b79bc7bb2c9e54776cba2af9
LluisDynasoft <luis.parets@grupodynasoft.com>
2025-10-06 09:50:00 +0200

    Create create-zip.sh

---
+++ create-zip.sh
@@ -0,0 +1,13 @@
+---
+
+
+### `create-zip.sh` (Linux/macOS)
+
+
+```bash
+#!/usr/bin/env bash
+set -e
+pkg_name="copilot-dotnet-starter.zip"
+rm -f "$pkg_name"
+zip -r "$pkg_name" .github README.md
+echo "Created $pkg_name"
